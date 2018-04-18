835189c41123e1017845385ac3bf640db8b7e47f
Larry Price <larry.price.dev@gmail.com>
2018-04-17 20:26:08 -0500

    Lesson 2

--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type SearchPage struct {
+	Query   string
+	Results []Result
+}
+
+type Result struct {
+	Title  string
+	Author string
+	Year   int
+	ID     string
+}
+
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		results := []Result{
+			Result{"1984", "Orwell", 1950, "123"},
+			Result{"Persuasion", "Austen", 1817, "234"},
+			Result{"Holes", "Sachar", 2000, "345"},
+		}
+		p := SearchPage{Query: r.FormValue("search"), Results: results}
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Go Web Development\n")
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Println(http.ListenAndServe(":4000", nil))
